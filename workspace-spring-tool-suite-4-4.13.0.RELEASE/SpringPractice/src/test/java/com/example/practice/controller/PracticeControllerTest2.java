359970ec5c2ebbbc0804629d00c3b6a07f2c5e87
Takuya-Endo <above5purple@gmail.com>
2021-12-24 07:02:06 +0900

    [Update]SpringPractice-17

--- workspace-spring-tool-suite-4-4.13.0.RELEASE/SpringPractice/src/test/java/com/example/practice/controller/PracticeControllerTest2.java
+++ workspace-spring-tool-suite-4-4.13.0.RELEASE/SpringPractice/src/test/java/com/example/practice/controller/PracticeControllerTest2.java
@@ -6,13 +6,32 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.example.practice.service.TblUserService;
+
+import mockit.Injectable;
+import mockit.Tested;
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PracticeControllerTest2 {
-
+	
+	@Tested
+	@Autowired
+	private PracticeController practiceController;
+	
+	@Injectable
+	@Autowired
+	private TblUserService tblUserService;
+	
 	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	static void setUpBeforeClass() throws Exception {	
 	}
 
 	@AfterAll
@@ -21,6 +40,7 @@ class PracticeControllerTest2 {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		
 	}
 
 	@AfterEach
