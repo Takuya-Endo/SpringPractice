c2f219931dbe9970d1e2f6747d17b56a8de7403d
Takuya-Endo <above5purple@gmail.com>
2022-05-19 07:29:37 +0900

    [Update]SpringPractice-34

--- workspace-spring-tool-suite-4-4.13.0.RELEASE/SpringPractice/src/main/java/com/example/practice/controller/PracticeController.java
+++ workspace-spring-tool-suite-4-4.13.0.RELEASE/SpringPractice/src/main/java/com/example/practice/controller/PracticeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,7 +46,13 @@ public class PracticeController {
 	}
 	
 	@RequestMapping(value = "insert")
-	public String insert(@ModelAttribute User user) {
+	public String insert(@Validated @ModelAttribute User user, BindingResult bindingResult) {
+		
+		if (bindingResult.hasErrors()) {
+//			model.addAttribute("userForm", new User());
+//			return "html/insert";
+			return "forward:create";
+		}
 		
 		int id = this.tblUserService.getNextId();
 		user.setId(id);
