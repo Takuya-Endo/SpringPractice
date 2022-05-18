d25b64ce398cdf28651fca7e32582233e0ea5c96
Takuya-Endo <above5purple@gmail.com>
2022-05-18 22:11:16 +0900

    [Update]SpringPractice-33

--- workspace-spring-tool-suite-4-4.13.0.RELEASE/SpringPractice/src/main/java/com/example/practice/entity/User.java
+++ workspace-spring-tool-suite-4-4.13.0.RELEASE/SpringPractice/src/main/java/com/example/practice/entity/User.java
@@ -1,12 +1,14 @@
 package com.example.practice.entity;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 import lombok.Data;
 
 @Data
 public class User {
 	
-	private int id;
-	private String name;
-	private int age;
+	@NotEmpty private int id;
+	@NotEmpty private String name;
+	@NotEmpty private int age;
 	
 }
